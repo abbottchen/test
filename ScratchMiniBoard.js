de7ebcd2b2a632ede7befd9b24d5f0468ce87d33
abbottchen <53142807@qq.com>
2017-06-19 12:57:03 +0800

    Update ScratchMiniBoard.js

--- ScratchMiniBoard.js
+++ ScratchMiniBoard.js
@@ -309,10 +309,7 @@
 
   var blocks = [
     [' ', '设置 数字%m.DigitalIOName 脚为 %m.DigitalIOmode', 'digitalWrite', 'D1', 'on'],
-    [' ', '输出 %m.DigitalIOOutType 电平到 数字 %m.DigitalIOName 脚', 'digitalWrite', '低', 'D1'],
-    ['r', '数字脚 %m.DigitalIOName 脚 输入电平', 'digitalRead', 'D1'],
-    ['r', '模拟输入脚 %m.AnalogInPortName 脚 值', 'digitalRead', 'A1'],
-    [' ', '输出 %d ms(周期),占空比 d% (0~100%) 信号到模拟输出脚 %m.AnalogIOName', 'digitalRead', 0 , 0,'PWM1']
+    [' ', '输出 %m.DigitalIOOutType 电平到 数字 %m.DigitalIOName 脚', 'digitalWrite', '低', 'D1']
   ];
 
   var menus = {
@@ -320,14 +317,7 @@
   	DigitalIOmode:['输入','输出'],
   	DigitalIOOutType:['低','高'],
   	AnalogInPortName:['A1','A2','A3'],
-  	AnalogOutPortName:['PWM1','PWM2'],
-    digitalOutputs: DIGITAL_PINS,
-    analogOutputs: PWM_PINS,
-    digitalInputs: DIGITAL_PINS,
-    analogInputs: ANALOG_PINS,
-    outputs: ['on', 'off'],
-    ops: ['>', '=', '<'],
-    tiltDir: ['up', 'down', 'left', 'right']
+  	AnalogOutPortName:['PWM1','PWM2']
   };
 
   var descriptor = {
