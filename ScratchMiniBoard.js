bc8067dbe623abed2944e3eab29b927275277448
abbottchen <53142807@qq.com>
2017-06-19 12:55:17 +0800

    Update ScratchMiniBoard.js

--- ScratchMiniBoard.js
+++ ScratchMiniBoard.js
@@ -310,21 +310,9 @@
   var blocks = [
     [' ', '设置 数字%m.DigitalIOName 脚为 %m.DigitalIOmode', 'digitalWrite', 'D1', 'on'],
     [' ', '输出 %m.DigitalIOOutType 电平到 数字 %m.DigitalIOName 脚', 'digitalWrite', '低', 'D1'],
-    ['r', '数字脚 %m.DigitalIOName 脚的输入电平', 'digitalRead', 'D1'],
+    ['r', '数字脚 %m.DigitalIOName 脚 输入电平', 'digitalRead', 'D1'],
     ['r', '模拟输入脚 %m.AnalogInPortName 脚 值', 'digitalRead', 'A1'],
-    [' ', '输出 %d ms(周期),占空比 d%(0~100%) 信号到模拟输出脚 %m.AnalogIOName', 'digitalRead', 0,0，'PWM1'],
-    [' ', 'set pin %d.analogOutputs to %n%', 'analogWrite', 9, 100],
-    ['h', 'when pin %d.digitalInputs is %m.outputs', 'whenDigitalRead', 9, 'on'],
-    ['b', 'pin %d.digitalInputs on?', 'digitalRead', 9],
-    ['-'],
-    ['h', 'when analog pin %d.analogInputs %m.ops %n%', 'whenAnalogRead', 'A0', '>', 50],
-    ['r', 'read analog pin %d.analogInputs', 'analogRead', 'A0'],
-    ['-'],
-    ['h', 'when shaken', 'whenIMUEvent'],
-    ['r', 'tilt angle %m.tiltDir', 'getTilt', 'up'],
-    ['-'],
-    [' ', 'set pin %d.digitalOutputs servo to %n degrees', 'rotateServo', 7, 90],
-    ['r', 'pin %d.digitalOutputs servo position', 'servoPosition', 7]
+    [' ', '输出 %d ms(周期),占空比 d% (0~100%) 信号到模拟输出脚 %m.AnalogIOName', 'digitalRead', 0 , 0,'PWM1'],
   ];
 
   var menus = {
