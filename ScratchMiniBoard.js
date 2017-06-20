88edb60df4e4b2296e2ef48862f5f405b7a4bec1
abbottchen <53142807@qq.com>
2017-06-20 23:02:08 +0800

    Update ScratchMiniBoard.js

--- ScratchMiniBoard.js
+++ ScratchMiniBoard.js
@@ -119,18 +119,8 @@
    ext.SetDigitPortLevel = function(level,which) { return SetDigitIoPortLevel(which,level); };	
 
    function SetPWMToPram(period,width,ch){ 
-	//console.log('ch:'+ch);    
-	//console.log('period:'+period);   
-	//console.log('Width:'+width);
-	   
 	period=period*1000;
-	//console.log('period:'+period);   
-	//console.log('Width:'+width);
-	   
 	Math.round(period);
-	//console.log('period:'+period);   
-	//console.log('Width:'+width);
-	   
 	if(period>65535)
 	 	period=65535;
 	else if(period<0)
@@ -142,9 +132,6 @@
 	console.log('period:'+period);   
 	console.log('Width:'+tmp);   
 	
-	//console.log('VarAnalogOutPortPeriod[ch]:'+VarAnalogOutPortPeriod[ch]); 
-	//console.log('VarAnalogOutPortWidth[ch]:'+VarAnalogOutPortWidth[ch]); 
-	   
 	VarAnalogOutPortPeriod[ch]=period;
 	VarAnalogOutPortWidth[ch]=tmp;
 	SendFrameToUart();  
@@ -292,7 +279,8 @@
             [' ', '输出 %m.DigitalIOOutType 电平到 数字 %m.DigitalIOName 脚', 'SetDigitPortLevel', '低', 'D1'],
             ['r', '数字脚 %m.DigitalIOName 脚 输入电平', 'sensor', 'D1'],
             ['r', '模拟输入脚 %m.AnalogInPortName 脚采样值', 'sensor', 'A1'],
-            [' ', '输出 %n ms的周期, %n (0~100%)占空比的信号到模拟输出脚 %m.AnalogOutPortName', 'SetPWMPram', 50 , 50 ,'PWM1']
+            [' ', '输出 %n ms的周期, %n (0~100%)占空比的信号到模拟输出脚 %m.AnalogOutPortName', 'SetPWMPram', 50 , 50 ,'PWM1'],
+	    [' ', '输出 %n (0~360)角度到模拟输出脚 %m.AnalogOutPortName (舵机)', 'SetServo', 180 ,'PWM1']
         ],
         menus: {
             DigitalIOName:['D1','D2','D3','D4','D5','D6'],
