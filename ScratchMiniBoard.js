d8824369eaee6adaa611a2743a5f8b27f37b6ac5
abbottchen <53142807@qq.com>
2017-06-21 12:38:59 +0800

    Update ScratchMiniBoard.js

--- ScratchMiniBoard.js
+++ ScratchMiniBoard.js
@@ -339,7 +339,7 @@
             ['r', '模拟输入脚 %m.AnalogInPortName 脚采样值', 'sensor', 'A1'],
             [' ', '输出 %n ms的周期, %n (0~100%)占空比的信号到模拟输出脚 %m.AnalogOutPortName', 'SetPWMPram', 50 , 50 ,'PWM1'],
 	    [' ', '输出 %n (0~360)角度到模拟输出脚 %m.AnalogOutPortName (舵机)', 'SetServo', 90 ,'PWM1'],
-	    ['R', '%m.WeatherDataType in %s', 'getWeather', '温度', 'Boston, MA'],	
+	    ['R', '%m.WeatherDataType in %s', 'getWeather', '温度', 'Beijing'],	
         ],
         menus: {
             DigitalIOName:['D1','D2','D3','D4','D5','D6'],
