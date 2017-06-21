bfd0161b9baaf43cfd5df154759d85634e9fb61e
abbottchen <53142807@qq.com>
2017-06-21 20:24:49 +0800

    Update ScratchMiniBoard.js

--- ScratchMiniBoard.js
+++ ScratchMiniBoard.js
@@ -395,7 +395,7 @@ function GetLeiweiValue(json , device, sensortype, sensorname) {
             [' ', '输出 %n ms的周期 %n (0~100%)占空比的信号到模拟输出脚 %m.AnalogOutPortName', 'SetPWMPram', 50 , 50 ,'PWM1'],
 	    [' ', '输出 %n (0~360)角度到模拟输出脚 %m.AnalogOutPortName (舵机)', 'SetServo', 100 ,'PWM1'],
 	    ['R', '%m.WeatherDataType 值 %s', 'getWeather', '温度', 'Beijing'],
-	    ['R', '获取乐为物联APPID %n 设备名称 %n  %m.SensorType 名称 %n 的值','GetLewei', '0000' ,'01' ,'传感器', '000']
+	    //['R', '获取乐为物联APPID %n 设备名称 %n  %m.SensorType 名称 %n 的值','GetLewei', '0000' ,'01' ,'传感器', '000']
         ],
         menus: {
             DigitalIOName:['D1','D2','D3','D4','D5','D6'],
