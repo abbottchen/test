e4ffb235688385d1b9cc9b27351e32eba42b9b38
abbottchen <53142807@qq.com>
2017-06-30 12:25:17 +0800

    Update ScratchMiniBoard.js

--- ScratchMiniBoard.js
+++ ScratchMiniBoard.js
@@ -538,7 +538,7 @@ ext.SetLewei = function(appid , device, sensortype, sensorname, json) {
 	    [' ', '输出 %n (0~360)角度到模拟输出脚 %m.AnalogOutPortName (舵机)', 'SetServo', 180 ,'PWM1'],
 	    ['R', '%s %m.WeatherDataType 值 ', 'getWeather', 'Beijing', '温度'],
 	    ['R', '获取乐为物联APPID %s 设备名称 %s  %m.SensorType 名称 %s 的值','GetLewei', 'da34db80af9c46669159fe8982bbdbe0' ,'ban1' ,'传感器', '湿度'],
-	    [' ', '设置乐为物联APPID %s 设备名称 %s  %m.SensorType 名称 %s 的值为 %n ','SetLewei', 'da34db80af9c46669159fe8982bbdbe0' ,'ban1' ,'传感器', '湿度','10']
+	    [' ', '设置乐为物联APPID %s 设备名称 %s  %m.SensorType 名称 %s 的值为 %n ','SetLewei', 'da34db80af9c46669159fe8982bbdbe0' ,'ban1' ,'传感器', '湿度','5']
         ],
         menus: {
             DigitalIOName:['D1','D2','D3','D4','D5','D6'],
