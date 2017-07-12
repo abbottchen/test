725d6c518e96d1624a200eeb142bd8bdffb33c0e
abbottchen <53142807@qq.com>
2017-07-12 21:23:04 +0800

    Update ScratchMiniBoard.js

--- ScratchMiniBoard.js
+++ ScratchMiniBoard.js
@@ -255,25 +255,7 @@
         }, 500);
     };
 	
-    ext.resetAll = function(){
-	setInterval("GetYeelinkpoll()", 10000);
-    };	
-
-    ext._deviceRemoved = function(dev) {
-        if(device != dev) return;
-        device = null;
-    };
-
-    ext._shutdown = function() {
-        if(device) device.close();
-        device = null;
-    };
-
-    ext._getStatus = function() {
-        //if(!device) return {status: 1, msg: 'ScratchMiniBoard disconnected'};
-        //if(watchdog) return {status: 1, msg: 'Probing for ScratchMiniBoard'};
-        return {status: 2, msg: 'ScratchMiniBoard connected'};
-    }
+  
     /******************************************************/
     var cacheDuration = 1800000 //ms, 30 minutes
     var cachedTemps = {};
@@ -503,7 +485,29 @@ ext.SetYeelink = function(appid,device,sensor,value){
 	AJAX_JSON(yurl,5000,function(data){
 		console.log("设置完成")
 	});
-};	
+};
+	
+/******************************************************/	
+  ext.resetAll = function(){
+	setInterval("GetYeelinkpoll()", 10000);
+    };	
+
+    ext._deviceRemoved = function(dev) {
+        if(device != dev) return;
+        device = null;
+    };
+
+    ext._shutdown = function() {
+        if(device) device.close();
+        device = null;
+    };
+
+    ext._getStatus = function() {
+        //if(!device) return {status: 1, msg: 'ScratchMiniBoard disconnected'};
+        //if(watchdog) return {status: 1, msg: 'Probing for ScratchMiniBoard'};
+        return {status: 2, msg: 'ScratchMiniBoard connected'};
+    }	
+	
 /******************************************************/
     var descriptor = {
         blocks: [
@@ -515,7 +519,7 @@ ext.SetYeelink = function(appid,device,sensor,value){
 	    [' ', '输出 %n (0~360)角度到模拟输出脚 %m.AnalogOutPortName (舵机)', 'SetServo', 90 ,'PWM1'],
 	    ['R', 'APPID %s 城市%s %m.WeatherDataType 值 ', 'getWeather', '960f7f58abbc5c98030d1899739c1ba8','Beijing', '温度'],
 	    ['R', '获取乐为物联APPID %s 设备标识为 %s  传感器标识为 %s 的值','GetLewei', 'bed12be663' ,'01' , 'Humidity'],
-	    [' ', '设置乐为物联APPID %s 设备标识为 %s  传感器标识为 %s 的值为 %n ','SetLewei', 'bed12be663' ,'01' ,'Humidity','10'],
+	    [' ', '设置乐为物联APPID %s 设备标识为 %s  传感器标识为 %s 的值为 %n ','SetLewei', 'bed12be663' ,'01' ,'Humidity','20'],
             ['R', '获取Yeelink设备为 %s  传感器为 %s 的值','GetYeelink','12094' ,'403236'],
 	    [' ', '设置Yeelink apikey %s 设备为 %s  传感器为 %s 的值为 %n','SetYeelink','57f36d198515f6e4c090187c4c9ab54b','12094' ,'403236','0']
 	],
