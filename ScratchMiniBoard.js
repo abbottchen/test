7a862fe17d67b877152678f3f689c8cbbcabc3a6
abbottchen <53142807@qq.com>
2017-07-15 10:57:07 +0800

    Update ScratchMiniBoard.js

--- ScratchMiniBoard.js
+++ ScratchMiniBoard.js
@@ -408,65 +408,36 @@ ext.SetLewei = function(appid , idName, sensorid, value) {
 /******************************************************/ 
 /*获取和设置Yeelink上的传感器数据*/
 /******************************************************/  
-//获取Yeelink的数据
-function GetYeelinkData(sn,device,sensor,callback){
-	var	yeelinkurl='http://api.yeelink.net/v1.0/device/'+device+'/sensor/'+sensor+'/datapoints'
-	var url=VPS_url+'get'
-	+'?&u='+yeelinkurl
-	+'&h='
-	+'&b=';
-	//console.log(url);
+var YeelinkCached = {};	
+ext.GetYeelink = function(device,sensor,callback) {
+	var	yeelinkurl='http://localhost:9000/yeelink/'+device+'/sensor/'+sensor+'/datapoint'
+	var sensorname={device:sensor};
+	if (sensorname in YeelinkCached){
+		var time=Date.now() - YeelinkCached[sensorname].time;
+		console.log('当前时间:'+Date.now());
+        	console.log('记录时间:'+YeelinkCached[sensorname].time);	
+        	console.log('时间差:'+time);	
+		if(time<5000){//5000s内的读取操作，为读取缓冲区
+			console.log('取缓冲区:'+YeelinkCached[sensorname].data); 
+			callback(YeelinkCached[sensorname].data);
+			return;
+		}
+    	}
+      	
 	$.ajax({ 
-    	url: url,
-      	timeout:8000,
-     	type: 'GET',
-     	dataType: 'json',
-      	success: function(data) { 
-			callback(sn,data);
-      	},
-      	error: function(XMLHttpRequest, textStatus){
-			console.log('Error:'+textStatus);
-		},
-  	});	
-}
-
-var YeelinkData =new Array();
-//获取Yeelink的数据
-function GetYeelinkpoll(){
-	//console.log(Date.now()+' YeelinkData.length:'+YeelinkData.length);
-	for(var i=0;i<YeelinkData.length;i++){
-		GetYeelinkData(i,YeelinkData[i].device,YeelinkData[i].sensor,function(sn,data){
-			YeelinkData[sn].value=data.value;
-			YeelinkData[sn].time=Date.now();
-			console.log('YeelinkData['+sn+'].value:'+YeelinkData[sn].value);
-		});
-	}
-}
-	
-ext.GetYeelink = function(device,sensor) {
-	var	yeelinkurl='http://api.yeelink.net/v1.0/device/'+device+'/sensor/'+sensor+'/datapoints'
-	var url=VPS_url+'get'
-	+'?&u='+yeelinkurl
-	+'&h='
-	+'&b=';
-	console.log(url);
-	var ret;
-	$.ajax({ 
-    		url: url,
-    		async: false,
-		timeout:8000,
+		url: yeelinkurl,
+		timeout:5000,
 		type: 'GET',
 		dataType: 'json',
 		success: function(data) { 
-			ret=parseFloat(data.value);
+			console.log('Yeelink传感器值:'+data.value);
+			YeelinkCached[sensorname] = {data: data.value, time: Date.now()};
+			callback(data.value);
 		},
 		error: function(XMLHttpRequest, textStatus){
-				console.log('Error:'+textStatus);
-				//ret="";
-			},
-		});	
-  		//console.log('Ajax return:'+ret);
-   	 return ret;
+			console.log('Error:'+textStatus);
+		},
+ 	});	
 }	
 
 ext.SetYeelink = function(appid,device,sensor,value){
@@ -517,7 +488,7 @@ ext.SetYeelink = function(appid,device,sensor,value){
 	    ['R', 'APPID %s 城市%s %m.WeatherDataType 值 ', 'getWeather', '960f7f58abbc5c98030d1899739c1ba8','Beijing', '温度'],
 	    ['R', '获取乐为物联APPID %s 设备标识为 %s  传感器标识为 %s 的值','GetLewei', 'bed12be663' ,'01' , 'Humidity'],
 	    [' ', '设置乐为物联APPID %s 设备标识为 %s  传感器标识为 %s 的值为 %n ','SetLewei', 'bed12be663' ,'01' ,'Humidity','11'],
-            ['r', '获取Yeelink设备为 %s  传感器为 %s 的值','GetYeelink','12094' ,'403236'],
+            ['R', '获取Yeelink设备为 %s  传感器为 %s 的值','GetYeelink','12094' ,'403236'],
 	    [' ', '设置Yeelink apikey %s 设备为 %s  传感器为 %s 的值为 %n','SetYeelink','57f36d198515f6e4c090187c4c9ab54b','12094' ,'403236','0']
 	],
         menus: {
